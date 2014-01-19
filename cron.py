6ccaf5e3d10ed846edc96a8adc9f3e69a47cd7a9
frodopwns <frodopwns@gmail.com>
2014-01-18 17:05:25 -0700

    removing something i didn't use

--- cron.py
+++ cron.py
@@ -3,13 +3,6 @@ from valar.utils import get_hosts, send_mail
 from valar.pycgm import CgminerAPI
 
 
-def get_summaries(hosts):
-    results = {}
-    for h in hosts:
-        cgm = CgminerAPI(host=h)
-        results[h] = cgm.summary()
-    return results
-
 if __name__ == '__main__':
     result = False
     hosts = get_hosts()
