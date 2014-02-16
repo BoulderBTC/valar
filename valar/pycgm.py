4de9f62e579573a81832b22d90dc49499fbca33f
Erin Corson <erin.corson@colorado.edu>
2014-02-16 12:22:16 -0700

    removing send_mail functionality

--- valar/pycgm.py
+++ valar/pycgm.py
@@ -1,14 +1,14 @@
 import socket
 import json
 import logging
-import smtplib
-from email.mime.text import MIMEText
+
 from valar import valar_settings as settings
 
 class CgminerAPI(object):
     """ Cgminer RPC API wrapper. """
     def __init__(self, host='localhost', port=4028):
         self.data = {}
+        self.ok = True
         self.host = host
         self.port = port
 
@@ -29,9 +29,7 @@ class CgminerAPI(object):
             sock.send(json.dumps(payload))
             received = self._receive(sock)
         except:
-            subject = "{0} - ERROR".format(self.host)
-            message = "{0} - Error\n\n{1}".format(self.host, "timeout")
-            send_mail(subject, message)
+            self.ok = False
         finally:
           #avoid error on OSX
           try:
@@ -63,15 +61,4 @@ class CgminerAPI(object):
             return self.command(attr, arg)
         return out
 
-def send_mail(subject, message):
-    username = settings.gmail_user
-    password = settings.gmail_password
-    server = smtplib.SMTP('smtp.gmail.com:587')
-    server.starttls()
-    server.login(username,password)
-    msg = MIMEText(message)
-    sender = settings.email_sender
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = ", ".join(settings.toaddrs)
-    server.sendmail(sender, settings.toaddrs, msg.as_string())
+
