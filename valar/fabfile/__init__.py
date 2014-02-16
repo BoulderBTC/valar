67cc903b019cf2722e072779b765314373f3e6a3
Erin Corson <erin.corson@colorado.edu>
2014-02-16 13:24:35 -0700

    adding a path to allow improt to work

--- valar/fabfile/__init__.py
+++ valar/fabfile/__init__.py
@@ -1,7 +1,11 @@
-from valar_settings import ssh_user, ssh_password
+import os, sys
 from fabric.api import *
+sys.path.append(os.path.abspath("../"))
+from valar.valar_settings import ssh_user, ssh_password
 env.user = ssh_user
 env.password = ssh_password
+
+
 def restart(host):
     with settings(host_string=host):
         run("hostname; pwd; ls -al")
