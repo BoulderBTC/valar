515512bc99fc969e46bc249c6a97da15238e43ba
frodopwns <frodopwns@gmail.com>
2014-02-18 23:07:44 -0700

    adding ssh user and password to example settings file

--- valar/example.valar_settings.py
+++ valar/example.valar_settings.py
@@ -3,6 +3,9 @@ gmail_password = 'gmail application specific password'
 toaddrs = ['recipient1@example.com', 'recipient2@example.com']
 email_sender = 'youruser@gmail.com'
 valar_api = 'http://127.0.0.1:5005/'
+ssh_user = "sshuser"
+ssh_password = "Password"
+
 hosts = [
     {'hostname': '10.0.0.10', 'name': 'gandalf'},
     {'hostname': '10.0.0.11', 'name': 'frodo'}
