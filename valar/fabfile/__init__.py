99e9231f68c335e41c2e78b084fc13d58f429459
Erin Corson <erin.corson@colorado.edu>
2014-02-18 22:40:04 -0700

    making restart function work

--- valar/fabfile/__init__.py
+++ valar/fabfile/__init__.py
@@ -1,11 +1,17 @@
 import os, sys
 from fabric.api import *
 sys.path.append(os.path.abspath("../"))
+
 from valar.valar_settings import ssh_user, ssh_password
+from valar.utils import find_sick_workers
 env.user = ssh_user
 env.password = ssh_password
 
 
 def restart(host):
     with settings(host_string=host):
-        run("hostname; pwd; ls -al")
+        sudo("/sbin/reboot", shell=False)
+
+
+
+    
