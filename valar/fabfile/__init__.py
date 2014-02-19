f96a1243fd2ce969af161ca12e66bc4e884fe0d0
Erin Corson <erin.corson@colorado.edu>
2014-02-18 22:44:23 -0700

    removing junk

--- valar/fabfile/__init__.py
+++ valar/fabfile/__init__.py
@@ -3,7 +3,7 @@ from fabric.api import *
 sys.path.append(os.path.abspath("../"))
 
 from valar.valar_settings import ssh_user, ssh_password
-from valar.utils import find_sick_workers
+
 env.user = ssh_user
 env.password = ssh_password
 
